95536de1c5e486d2150aaea5e6a79ec3c0f76e59
whuhit <whuhit09@gmail.com>
2020-07-22 06:51:22 +0800

    fcn test

--- FCN/evalution_segmentaion.py
+++ FCN/evalution_segmentaion.py
@@ -5,30 +5,8 @@ import six
 
 
 def calc_semantic_segmentation_confusion(pred_labels, gt_labels):
-    """Collect a confusion matrix.
-
-    The number of classes :math:`n\_class` is
-    :math:`max(pred\_labels, gt\_labels) + 1`, which is
-    the maximum class id of the inputs added by one.
-
-    Args:
-        pred_labels (iterable of numpy.ndarray): A collection of predicted
-            labels. The shape of a label array
-            is :math:`(H, W)`. :math:`H` and :math:`W`
-            are height and width of the label.
-        gt_labels (iterable of numpy.ndarray): A collection of ground
-            truth labels. The shape of a ground truth label array is
-            :math:`(H, W)`, and its corresponding prediction label should
-            have the same shape.
-            A pixel with value :obj:`-1` will be ignored during evaluation.
-
-    Returns:
-        numpy.ndarray:
-        A confusion matrix. Its shape is :math:`(n\_class, n\_class)`.
-        The :math:`(i, j)` th element corresponds to the number of pixels
-        that are labeled as class :math:`i` by the ground truth and
-        class :math:`j` by the prediction.
-
+    """
+    Collect a confusion matrix.
     """
     pred_labels = iter(pred_labels)     # (352, 480)
     gt_labels = iter(gt_labels)     # (352, 480)
@@ -55,7 +33,7 @@ def calc_semantic_segmentation_confusion(pred_labels, gt_labels):
             n_class = lb_max + 1
             confusion = expanded_confusion
 
-        # Count statistics from valid pixels.  极度巧妙 × class_nums 正好使得每个ij能够对应.
+        # Count statistics from valid pixels.
         mask = gt_label >= 0
         confusion += np.bincount(
             n_class * gt_label[mask].astype(int) + pred_label[mask],
@@ -71,29 +49,6 @@ def calc_semantic_segmentation_confusion(pred_labels, gt_labels):
 
 
 def calc_semantic_segmentation_iou(confusion):
-    """Calculate Intersection over Union with a given confusion matrix.
-
-    The definition of Intersection over Union (IoU) is as follows,
-    where :math:`N_{ij}` is the number of pixels
-    that are labeled as class :math:`i` by the ground truth and
-    class :math:`j` by the prediction.
-
-    * :math:`\\text{IoU of the i-th class} =  \
-        \\frac{N_{ii}}{\\sum_{j=1}^k N_{ij} + \\sum_{j=1}^k N_{ji} - N_{ii}}`
-
-    Args:
-        confusion (numpy.ndarray): A confusion matrix. Its shape is
-            :math:`(n\_class, n\_class)`.
-            The :math:`(i, j)` th element corresponds to the number of pixels
-            that are labeled as class :math:`i` by the ground truth and
-            class :math:`j` by the prediction.
-
-    Returns:
-        numpy.ndarray:
-        An array of IoUs for the :math:`n\_class` classes. Its shape is
-        :math:`(n\_class,)`.
-
-    """
     iou_denominator = (confusion.sum(axis=1) + confusion.sum(axis=0)
                        - np.diag(confusion))
     iou = np.diag(confusion) / iou_denominator
@@ -101,79 +56,13 @@ def calc_semantic_segmentation_iou(confusion):
 
 
 def eval_semantic_segmentation(pred_labels, gt_labels):
-    """Evaluate metrics used in Semantic Segmentation.
-
-    This function calculates Intersection over Union (IoU), Pixel Accuracy
-    and Class Accuracy for the task of semantic segmentation.
-
-    The definition of metrics calculated by this function is as follows,
-    where :math:`N_{ij}` is the number of pixels
-    that are labeled as class :math:`i` by the ground truth and
-    class :math:`j` by the prediction.
-
-    * :math:`\\text{IoU of the i-th class} =  \
-        \\frac{N_{ii}}{\\sum_{j=1}^k N_{ij} + \\sum_{j=1}^k N_{ji} - N_{ii}}`
-    * :math:`\\text{mIoU} = \\frac{1}{k} \
-        \\sum_{i=1}^k \
-        \\frac{N_{ii}}{\\sum_{j=1}^k N_{ij} + \\sum_{j=1}^k N_{ji} - N_{ii}}`
-    * :math:`\\text{Pixel Accuracy} =  \
-        \\frac \
-        {\\sum_{i=1}^k N_{ii}} \
-        {\\sum_{i=1}^k \\sum_{j=1}^k N_{ij}}`
-    * :math:`\\text{Class Accuracy} = \
-        \\frac{N_{ii}}{\\sum_{j=1}^k N_{ij}}`
-    * :math:`\\text{Mean Class Accuracy} = \\frac{1}{k} \
-        \\sum_{i=1}^k \
-        \\frac{N_{ii}}{\\sum_{j=1}^k N_{ij}}`
-
-    The more detailed description of the above metrics can be found in a
-    review on semantic segmentation [#]_.
-
-    The number of classes :math:`n\_class` is
-    :math:`max(pred\_labels, gt\_labels) + 1`, which is
-    the maximum class id of the inputs added by one.
-
-    .. [#] Alberto Garcia-Garcia, Sergio Orts-Escolano, Sergiu Oprea, \
-    Victor Villena-Martinez, Jose Garcia-Rodriguez. \
-    `A Review on Deep Learning Techniques Applied to Semantic Segmentation \
-    <https://arxiv.org/abs/1704.06857>`_. arXiv 2017.
-
-    Args:
-        pred_labels (iterable of numpy.ndarray): A collection of predicted
-            labels. The shape of a label array
-            is :math:`(H, W)`. :math:`H` and :math:`W`
-            are height and width of the label.
-            For example, this is a list of labels
-            :obj:`[label_0, label_1, ...]`, where
-            :obj:`label_i.shape = (H_i, W_i)`.
-        gt_labels (iterable of numpy.ndarray): A collection of ground
-            truth labels. The shape of a ground truth label array is
-            :math:`(H, W)`, and its corresponding prediction label should
-            have the same shape.
-            A pixel with value :obj:`-1` will be ignored during evaluation.
-
-    Returns:
-        dict:
-
-        The keys, value-types and the description of the values are listed
-        below.
-
-        * **iou** (*numpy.ndarray*): An array of IoUs for the \
-            :math:`n\_class` classes. Its shape is :math:`(n\_class,)`.
-        * **miou** (*float*): The average of IoUs over classes.
-        * **pixel_accuracy** (*float*): The computed pixel accuracy.
-        * **class_accuracy** (*numpy.ndarray*): An array of class accuracies \
-            for the :math:`n\_class` classes. \
-            Its shape is :math:`(n\_class,)`.
-        * **mean_class_accuracy** (*float*): The average of class accuracies.
-
+    """
     # Evaluation code is based on
     # https://github.com/shelhamer/fcn.berkeleyvision.org/blob/master/
     # score.py#L37
     """
 
-    confusion = calc_semantic_segmentation_confusion(
-        pred_labels, gt_labels)
+    confusion = calc_semantic_segmentation_confusion(pred_labels, gt_labels)  # 混淆矩阵(一个batch)
     iou = calc_semantic_segmentation_iou(confusion)     # (12, )
     pixel_accuracy = np.diag(confusion).sum() / confusion.sum()
     class_accuracy = np.diag(confusion) / (np.sum(confusion, axis=1) + 1e-10)
